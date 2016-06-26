18187fcdc19f1e2783028db40967ad4d983702fb
Nick Shek <alfshek@hotmail.com>
2016-06-26 10:02:25 +0800

    setup a very simple test environment first

---
+++ tests/Middleware/LogAfterRequestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaravelRequest\Test\Middleware;
+use Orchestra\Testbench\TestCase;
+
+class LogAfterRequestTest extends TestCase{
+  public function setUp()
+  {
+    parent::setUp();
+  }
+
+  public function testSample()
+  {
+      $this->assertTrue(true);
+  }
+}
