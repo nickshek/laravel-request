e8142c493a8fd696cfea40964986a1894765ee41
Nick Shek <alfshek@hotmail.com>
2016-06-26 16:32:26 +0800

    add LaravelRequestProvider for backward compatibility

---
+++ src/LaravelRequestProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelRequest;
+
+/**
+ * @deprecated Please use \LaravelRequest\LaravelRequestServiceProvider.
+ */
+class LaravelRequestProvider extends LaravelRequestServiceProvider{
+
+}
